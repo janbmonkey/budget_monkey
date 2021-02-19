8890b5f53a12ffebbc7960a094cb5da92f46e4f3
IXTO\qnguyen <qnguyen@ixto.de>
2021-02-19 12:19:19 +0100

    Gateway setup

--- bmgateway/src/main/java/com/example/BMGateway/BmGatewayApplication.java
+++ bmgateway/src/main/java/com/example/BMGateway/BmGatewayApplication.java
@@ -2,11 +2,24 @@ package com.example.BMGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BmGatewayApplication {
 
+	@RequestMapping(value = "/")
+	public String getHelloGateway(){
+		return "Hello Baby Monkey";
+	}
 
+	@RequestMapping(value = "/ge")
+	public String getGetGaway(){
+		return "getGetGaway";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BmGatewayApplication.class, args);
